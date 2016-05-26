aae2588a1868bd4f5990e1c5a0fde80703cdf96a
VolkovaYulichka <Marchenko197@mail.ru>
2016-05-26 09:14:39 +0300

    Create Zavd_1.9.c

---
+++ Zavd_1.9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int mas[9][9], i, j;
+		for(i = 0; i < 9; i++) {
+		for( j = 0; j < 9; j++){
+			if(j <= 9 -i) 
+				mas[i][j] = 1;
+				else
+					mas[i][j] = 0;
+		}
+	}
+	for(i = 0; i < 9; i++) {
+			for(j = 0; j < 9; j++)
+			printf(" %d", mas[i][j]);
+			printf("\n");
+	}
+	system("pause");
+	return 0;		
+}
